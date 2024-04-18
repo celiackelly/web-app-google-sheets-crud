6ba9788e881b13d682c0dffd5cba137512caa123
Celia Kelly <celiackelly@gmail.com>
2024-04-18 13:11:56 -0400

    Completed Edit Customer form, through Part 4 of tutorial

--- serverSideFunctions.js
+++ serverSideFunctions.js
@@ -16,3 +16,39 @@ function deleteById(id) {
   const rowNumber = positionIndex === -1 ? 0 : positionIndex + 2 //use 0 to get an error if no match
   ws.deleteRow(rowNumber)
 }
+
+function getCustomerById(id) {
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  const ws = ss.getSheetByName('Customers')
+  const customerIds = ws
+    .getRange(2, 1, ws.getLastRow() - 1, 1)
+    .getValues()
+    .map(row => row[0].toString().toLowerCase())
+  const positionIndex = customerIds.indexOf(id.toString().toLowerCase())
+  const rowNumber = positionIndex === -1 ? 0 : positionIndex + 2
+
+  const customerInfo = ws.getRange(rowNumber, 1, 1, 4).getValues()[0]
+  return {
+    customerId: customerInfo[0],
+    firstName: customerInfo[1],
+    lastName: customerInfo[2],
+    phone: customerInfo[3],
+  }
+}
+
+function editCustomerbyId(id, customerInfo) {
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  const ws = ss.getSheetByName('Customers')
+  const customerIds = ws
+    .getRange(2, 1, ws.getLastRow() - 1, 1)
+    .getValues()
+    .map(row => row[0].toString().toLowerCase())
+  const positionIndex = customerIds.indexOf(id.toString().toLowerCase())
+  const rowNumber = positionIndex === -1 ? 0 : positionIndex + 2
+
+  ws.getRange(rowNumber, 2, 1, 3).setValues([
+    [customerInfo.firstName, customerInfo.lastName, customerInfo.phone],
+  ]) //get the row to edit and set new values
+
+  return true
+}
